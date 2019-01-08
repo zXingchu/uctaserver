3a9eeb0db54f35f22c122ff77e9f9327d55c4730
zhangxc <141250196@smail.nju.edu.cn>
2019-01-08 20:16:00 +0800

    Initial commit

---
+++ src/main/java/com/njuse/uctaserver/UctaserverApplication.java
@@ -0,0 +1,36 @@
+package com.njuse.uctaserver;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.SocketUtils;
+
+@SpringBootApplication
+public class UctaserverApplication {
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(80);
+        return connector;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(UctaserverApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/njuse/uctaserver/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.njuse.uctaserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+
+    @RequestMapping(value = "/isSuccess", method = RequestMethod.GET)
+    public @ResponseBody
+    String isSuccess(HttpServletRequest request) {
+        return "Success";
+    }
+
+}
