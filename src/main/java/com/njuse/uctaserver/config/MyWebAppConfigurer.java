6dc132cb49dab0aa7b69285c7ac61b1b7140b900
vapid33 <297141009@qq.com>
2019-03-21 20:59:27 +0800

    config resourceHandler

---
+++ src/main/java/com/njuse/uctaserver/config/MyWebAppConfigurer.java
@@ -0,0 +1,16 @@
+package com.njuse.uctaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by lenovo on 2019-03-21.
+ */
+@Configuration
+public class MyWebAppConfigurer implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/image/**").addResourceLocations("/home/img");
+	}
+}
